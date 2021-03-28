2d01620e965752fe3c67a6fa8d319d3eb6e90c52
Khoatrando <khoatrando516@gmail.com>
2021-03-28 23:23:15 +0700

    create index on server

--- Server/index.js
+++ Server/index.js
@@ -1,1 +1,11 @@
-console.log("asd")
+const express = require("express");
+const app= express();
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
+
+const PORT = process.env.PORT || 6001;
+
+app.listen(PORT,()=> console.log(`Server running on port :${PORT}`))
